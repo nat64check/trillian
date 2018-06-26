55282dadf34cc79c796c7b01c2c218945eeef1e3
Sander Steffann <sander@steffann.nl>
2018-06-26 17:48:16 +0200

    Fix InstanceRunResult api and bump version

--- measurements/urls.py
+++ measurements/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from rest_framework import routers
 
-from measurements.api.views import InstanceRunViewSet
+from measurements.api.views import InstanceRunResultsViewSet, InstanceRunViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 measurements_router = routers.SimpleRouter()
 measurements_router.register('instanceruns', InstanceRunViewSet, base_name='instancerun')
+measurements_router.register('instancerunresults', InstanceRunResultsViewSet, base_name='instancerunresult')
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
--- trillian_be/__init__.py
+++ trillian_be/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from uwsgi_tasks import set_uwsgi_callbacks
 
