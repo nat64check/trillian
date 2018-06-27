59f32b0a0be071ecd043f1caab1c49c33310578e
Sander Steffann <sander@steffann.nl>
2018-06-27 14:02:24 +0200

    Make the create-database script retry on failure Sometimes the script is too fast and starts talking to the server before it has had time to start up

--- create-database.sh
+++ create-database.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-set -e
 
-psql -v ON_ERROR_STOP=0 --username "postgres" --dbname "postgres" <<-EOSQL
-    CREATE USER trillian WITH PASSWORD '${PGPASSWORD}';
-    ALTER USER trillian WITH PASSWORD '${PGPASSWORD}';
-    CREATE DATABASE trillian WITH OWNER trillian TEMPLATE template_postgis;
+n=0
+until [ ${n} -ge 3 ]
+   do
+    psql -v ON_ERROR_STOP=0 --username "postgres" --dbname "postgres" <<-EOSQL && break
+        CREATE USER trillian WITH PASSWORD '${PGPASSWORD}';
+        ALTER USER trillian WITH PASSWORD '${PGPASSWORD}';
+        CREATE DATABASE trillian WITH OWNER trillian TEMPLATE template_postgis;
 EOSQL
+
+    echo "Retrying in 5" >&2
+    n=$[$n+1]
+    sleep 5
+done
