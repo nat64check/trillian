eec9a09fecf71548c29ab0f7b050ce67bec55b8f
Sander Steffann <sander@steffann.nl>
2018-07-07 21:26:19 +0200

    Fix schadowing of global

--- trillian_be/context_processors.py
+++ trillian_be/context_processors.py
@@ -1,4 +1,4 @@
 # noinspection PyUnusedLocal
 def app_version(request):
-    from . import __version__ as app_version
-    return {'APP_VERSION': app_version}
+    from . import __version__ as my_app_version
+    return {'APP_VERSION': my_app_version}
