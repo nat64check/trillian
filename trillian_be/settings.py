b911d524a2cca8ceed0a8a8dab9f20c7606bf27b
Sander Steffann <sander@steffann.nl>
2018-07-10 23:12:36 +0200

    Change default API permission to be more restrictive

--- trillian_be/settings.py
+++ trillian_be/settings.py
@@ -176,7 +176,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework_filters.backends.DjangoFilterBackend',
