bb809e80f59fff362c99567df1fb59ed93b4a329
Sander Steffann <sander@steffann.nl>
2018-07-12 15:11:11 +0200

    Forgot to add the authtoken extension and signals

--- generic/apps.py
+++ generic/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class GenericConfig(AppConfig):
     name = 'generic'
+
+    def ready(self):
+        # noinspection PyUnresolvedReferences
+        from . import signals
--- trillian_be/settings.py
+++ trillian_be/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'django_filters',
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_framework_filters',
     'rest_framework_swagger',
 
