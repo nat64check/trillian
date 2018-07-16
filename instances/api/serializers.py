ef26ac1ea90065f3891644da97de456b83fd87e1
Sander Steffann <sander@steffann.nl>
2018-07-16 18:02:38 +0200

    Split up Serializer for better nesting

--- instances/api/serializers.py
+++ instances/api/serializers.py
@@ -3,12 +3,17 @@ from rest_framework import serializers
 from instances.models import Marvin
 
 
-class MarvinSerializer(serializers.HyperlinkedModelSerializer):
+class PlainMarvinSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Marvin
-        fields = ('id', 'instance_type',
+        fields = ('instance_type',
                   'name', 'hostname',
                   'type', 'version',
                   'browser_name', 'browser_version',
                   'addresses',
-                  '_url')
+                  'first_seen', 'last_seen')
+
+
+class MarvinSerializer(PlainMarvinSerializer):
+    class Meta(PlainMarvinSerializer.Meta):
+        fields = ('id',) + PlainMarvinSerializer.Meta.fields + ('_url',)
